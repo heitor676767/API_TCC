60cdd07f2eb2c7e8de1a1160cb249aca5f62a2a4
heitor676767 <heitorjb10@gmail.com>
2026-09-02 16:48:28 -0300

    Correcao Metodo Registrar

--- Controllers/PetController.cs
+++ Controllers/PetController.cs
@@ -1,15 +1,38 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using ApiTCC.Data;
 using ApiTCC.Models;
+using ApiTCC.Utils;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace API_TCC.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class PetController
+    public class PetController : ControllerBase
     {
+        private readonly DataContext _context;
+        public readonly IConfiguration _configuration;
+
+        public PetController(DataContext context, IConfiguration configuration)
+        {
+            _context = context;
+            _configuration = configuration;
+        }
+
+        private async Task<bool> PetExistente(string rga)
+        {
+            if (await _context.TB_PETS.AnyAsync(x => x.Rga.ToLower() == rga.ToLower()))
+            {
+                return true;
+            }
+            return false;
+        }
+
         [HttpPost("Registrar")]
         public async Task<IActionResult> RegistrarPet(Pet pet)
         {
